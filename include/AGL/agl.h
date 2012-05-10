20671d076b4de2cf142a3f0d986797854bc40c06
Michael Williams - archie <m.t.williams@live.com>
2012-05-09 21:56:12 -0700

    Planning out AGL:   * Moved to a packet based system (blame void-)   * Added an untested wgl implementation

--- include/AGL/agl.h
+++ include/AGL/agl.h
@@ -27,6 +27,7 @@
 #ifndef _AGL_H_
 #define _AGL_H_
 
+#include <stdlib.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -73,45 +74,18 @@ AGL_EXPORT(uint32_t AGL_API aglInit( aglAllocCallback alloc_callback, aglFreeCal
 
 ////////////////////////////////////////////////////////////////////////////////
 
-typedef void (AGL_API *aglContextResized)( struct aglContext*, uint32_t width, uint32_t height );
-typedef void (AGL_API *aglContextClosed)( struct aglContext* );
-
 typedef struct aglContextSettings {
     uint32_t width;
     uint32_t height;
+    uint32_t samples;
     unsigned vsync      : 1;
     unsigned fullscreen : 1;
-    unsigned resizeable : 1;
-    unsigned reserved   : 5;
-    aglContextResized resized;
-    aglContextResized closed;
+    unsigned reserved   : 6;
 } aglContextSettings;
 
 typedef struct aglContext aglContext;
 
-AGL_EXPORT(void AGL_API aglSetContextTitle( aglContext* context, const char* title ));
-
-////////////////////////////////////////////////////////////////////////////////
-
-typedef uint32_t (AGL_API *aglCommandBufferCallback)( struct aglCommandBuffer*, const size_t );
-
-typedef struct aglCommandBuffer {
-    void* begin;
-    void* end;
-    void* current;
-    aglCommandBufferCallback callback;
-} aglCommandBuffer;
-
-typedef struct aglCompiledCommandBuffer aglCompiledCommandBuffer;
-
-AGL_EXPORT(aglCompiledCommandBuffer* AGL_API aglCompileCommandBuffer( const aglCommandBuffer* cmd_buffer ));
-
-AGL_EXPORT(void AGL_API aglExecuteCommands( aglContext* context, const aglCommandBuffer* cmd_buffer ));
-AGL_EXPORT(void AGL_API aglExecuteCompiledCommands( aglContext* context, const aglCompiledCommandBuffer* cmd_buffer ));
-
-////////////////////////////////////////////////////////////////////////////////
-
-AGL_EXPORT(aglContext* AGL_API aglCreateDeferredContext( aglCommandBuffer* cmd_buffer ));
+AGL_EXPORT(void AGL_API aglSwapBuffers( aglContext* context ));
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -148,14 +122,230 @@ typedef struct aglInputElementDesc {
 AGL_EXPORT(aglInputLayout* AGL_API aglCreateInputLayout( const size_t num_descs, const aglInputElementDesc* descs ));
 AGL_EXPORT(void AGL_API aglDestroyInputLayout( aglInputLayout* input_layout ));
 
-AGL_EXPORT(void AGL_API aglSetInputLayout( aglCommandBuffer* cmd_buffer, const aglInputLayout* input_layout ));
+////////////////////////////////////////////////////////////////////////////////
 
-typedef struct aglVertexBuffer   aglVertexBuffer;
-typedef struct aglIndexBuffer    aglIndexBuffer;
+typedef struct aglRasterizerState aglRasterizerState;
+
+typedef enum AGL_FILL_MODE {
+    AGL_FILL_WIREFRAME = 1,
+    AGL_FILL_SOLID     = 2
+} AGL_FILL_MODE;
+
+typedef enum AGL_CULL_MODE {
+    AGL_CULL_NONE      = 1,
+    AGL_CULL_FRONT     = 2,
+    AGL_CULL_BACK      = 3
+} AGL_CULL_MODE;
+
+typedef enum AGL_FRONT_FACE {
+    AGL_CLOCKWISE         = 1,
+    AGL_COUNTER_CLOCKWISE = 2
+} AGL_FRONT_FACE;
+
+typedef enum aglRasterizerDesc {
+    AGL_FILL_MODE  fill_mode;
+    AGL_CULL_MODE  cull_mode;
+    AGL_FRONT_FACE front_face;
+    unsigned       enable_scissor : 1;
+    unsigned       reserved : 7;
+} aglRasterizerDesc;
+
+AGL_EXPORT(aglRasterizerState* AGL_API aglCreateRasterizerState( const aglRasterizerDesc* desc ));
+AGL_EXPORT(void AGL_API aglDestroyRasterizerState( aglRasterizerState* rasterizer_state ));
+
+typedef enum AGL_BLEND {
+    AGL_BLEND_ZERO             = 1,
+    AGL_BLEND_ONE              = 2,
+    AGL_BLEND_SRC_COLOR        = 3,
+    AGL_BLEND_INV_SRC_COLOR    = 4,
+    AGL_BLEND_SRC_ALPHA        = 5,
+    AGL_BLEND_INV_SRC_ALPHA    = 6,
+    AGL_BLEND_DEST_ALPHA       = 7,
+    AGL_BLEND_INV_DEST_ALPHA   = 8,
+    AGL_BLEND_DEST_COLOR       = 9,
+    AGL_BLEND_INV_DEST_COLOR   = 10,
+    AGL_BLEND_SRC_ALPHA_SAT    = 11,
+    AGL_BLEND_BLEND_FACTOR     = 14,
+    AGL_BLEND_INV_BLEND_FACTOR = 15
+} AGL_BLEND;
+
+typedef enum AGL_BLEND_OP {
+    AGL_BLEND_OP_ADD          = 1,
+    AGL_BLEND_OP_SUBTRACT     = 2,
+    AGL_BLEND_OP_REV_SUBTRACT = 3,
+    AGL_BLEND_OP_MIN          = 4,
+    AGL_BLEND_OP_MAX          = 5 
+} AGL_BLEND_OP;
+
+typedef enum aglBlendDesc {
+    unsigned     enable_blend : 1;
+    unsigned     reserved     : 7;
+    AGL_BLEND    src_blend;
+    AGL_BLEND    dest_blend;
+    AGL_BLEND_OP blend_op;
+    AGL_BLEND    src_blend_alpha;
+    AGL_BLEND    dest_blend_alpha;
+    AGL_BLEND_OP blend_op_alpha;
+} aglBlendDesc;
+
+typedef struct aglBlendState aglBlendState;
+
+AGL_EXPORT(aglBlendState* AGL_API aglCreateBlendState( const aglBlendDesc* desc ));
+AGL_EXPORT(void AGL_API aglDestroyBlendState( aglBlendState* blend_state ));
+
+typedef enum AGL_COMPARISON_FUNC {
+    AGL_COMPARISON_NEVER         = 1,
+    AGL_COMPARISON_LESS          = 2,
+    AGL_COMPARISON_EQUAL         = 3,
+    AGL_COMPARISON_LESS_EQUAL    = 4,
+    AGL_COMPARISON_GREATER       = 5,
+    AGL_COMPARISON_NOT_EQUAL     = 6,
+    AGL_COMPARISON_GREATER_EQUAL = 7,
+    AGL_COMPARISON_ALWAYS        = 8 
+} AGL_COMPARISON_FUNC;
+
+typedef enum AGL_COMPARISON_FUNC {
+  AGL_COMPARISON_NEVER          = 1,
+  AGL_COMPARISON_LESS           = 2,
+  AGL_COMPARISON_EQUAL          = 3,
+  AGL_COMPARISON_LESS_EQUAL     = 4,
+  AGL_COMPARISON_GREATER        = 5,
+  AGL_COMPARISON_NOT_EQUAL      = 6,
+  AGL_COMPARISON_GREATER_EQUAL  = 7,
+  AGL_COMPARISON_ALWAYS         = 8 
+} AGL_COMPARISON_FUNC;
+
+typedef struct aglDepthStencilOpDesc {
+    AGL_STENCIL_OP stencil_fail;
+    AGL_STENCIL_OP stencil_depth_fail;
+    AGL_STENCIL_OP stencil_pass;
+    AGL_COMPARISON_FUNC stencil_func;
+} aglDepthStencilOpDesc;
+
+typedef struct aglDepthStencilDesc {
+    unsigned enable_depth_test   : 1;
+    unsigned enable_depth_write  : 1;
+    unsigned enable_stencil_test : 1;
+    unsigned reserved            : 6;
+    AGL_COMPARISON_FUNC depth_func;
+    aglDepthStencilOpDesc front_face;
+    aglDepthStencilOpDesc back_face;
+} aglDepthStencilDesc;
+
+typedef struct aglDepthStencilState aglDepthStencilState;
+
+AGL_EXPORT(aglDepthStencilState* AGL_API aglCreateDepthStencilState( const aglDepthStencilDesc* desc ));
+AGL_EXPORT(void AGL_API aglDestroyDepthstencilState( aglDepthStencilState* depth_stencil_state ));
+
+////////////////////////////////////////////////////////////////////////////////
+
+typedef enum AGL_USAGE {
+    AGL_USAGE_DEFAULT   = 0,
+    AGL_USAGE_IMMUTABLE = 1,
+    AGL_USAGE_DYNAMIC   = 2,
+    AGL_USAGE_STAGING   = 3 
+} AGL_USAGE;
+
+typedef enum AGL_CPU_ACCESS_FLAG {
+    AGL_CPU_ACCESS_WRITE   = 0x10000L,
+    AGL_CPU_ACCESS_READ    = 0x20000L 
+} AGL_CPU_ACCESS_FLAG;
+
+////////////////////////////////////////////////////////////////////////////////
+
+typedef struct aglVertexBuffer aglVertexBuffer;
+
+AGL_EXPORT(aglVertexBuffer* AGL_API aglCreateVertexBuffer( AGL_USAGE usage, uint32_t access_flags, size_t num_bytes, const void* data ));
+AGL_EXPORT(void AGL_API aglDestroyVertexBuffer( aglVertexBuffer* vertex_buffer ));
+
+typedef struct aglIndexBuffer aglIndexBuffer;
+
+AGL_EXPORT(aglIndexBuffer* AGL_API aglCreateIndexBuffer( AGL_USAGE usage, uint32_t access_flags, size_t num_bytes, const void* data ));
+AGL_EXPORT(void AGL_API aglDestroyIndexBuffer( aglIndexBuffer* index_buffer ));
+
 typedef struct aglConstantBuffer aglConstantBuffer;
 
+AGL_EXPORT(aglConstantBuffer* AGL_API aglCreateConstantBuffer( AGL_USAGE usage, uint32_t access_flags, size_t num_bytes, const void* data ));
+AGL_EXPORT(void AGL_API aglDestroyConstantBuffer( aglConstantBuffer* constant_buffer ));
+
+////////////////////////////////////////////////////////////////////////////////
+
+typedef struct aglTexture aglTexture;
+
+
+
+typedef struct aglRenderTarget aglRenderTarget;
+
+////////////////////////////////////////////////////////////////////////////////
+
 typedef struct aglVertexShader aglVertexShader;
-typedef struct aglPixelShader  aglPixelShader;
+typedef struct aglPixelShader aglPixelShader;
+
+////////////////////////////////////////////////////////////////////////////////
+
+#define AGL_INIT_CMD( type, st ) { memset((void*)&(st), 0, sizeof(agl##type##Command)); (st)._cmd.type = AGL_COMMAND_##type; }
+
+typedef enum AGL_COMMAND_TYPE {
+    AGL_COMMAND_Clear = 1,
+    AGL_COMMAND_Swap  = 2,
+    AGL_COMMAND_Draw  = 3,
+} AGL_COMMAND_TYPE;
+
+typedef struct aglCommand {
+    AGL_COMMAND_TYPE type : 8;
+} aglCommand;
+
+typedef AGL_CLEAR_BITS {
+    AGL_CLEAR_COLOR   = (1 << 0),
+    AGL_CLEAR_DEPTH   = (1 << 1),
+    AGL_CLEAR_STENCIL = (1 << 2)
+} AGL_CLEAR_BITS;
+
+typedef struct aglClearCommand {
+    aglCommand _cmd;
+    AGL_CLEAR_BITS bits : 8;
+    float   color[4];
+    float   depth;
+    uint8_t stencil;
+} aglClearCommand;
+
+typedef struct aglSwapCommand {
+    aglCommand _cmd;
+} aglSwapCommand;
+
+typedef enum AGL_DRAW_TYPE {
+    AGL_DRAW         = 1,
+    AGL_DRAW_INDEXED = 2,
+} AGL_DRAW_TYPE;
+
+typedef struct aglDrawCommand {
+    aglCommand _cmd;
+    aglRasterizerState*     rasterizer_state;
+    aglBlendState*          blend_state;
+    aglDepthStencilState*   depth_stencil_state;
+    aglRenderTarget*        render_target;
+    aglInputLayout*         input_layout;
+    AGL_PRIMITIVE_TOPOLOGY  primitive_topology : 8;
+    aglVertexShader*        vertex_shader;
+    aglPixelShader*         pixel_shader;
+    aglTexture*             textures[8];
+    aglIndexBuffer*         index_buffer;
+    aglVertexBuffer*        vertex_buffers[8];
+    AGL_DRAW_TYPE           draw_type : 8;
+    union params {
+        struct draw {
+            uint32_t num_vertices;
+            uint32_t start_vertex;
+        };
+
+        struct draw_indexed {
+            uint32_t num_indices;
+            uint32_t start_index;
+        };
+    };
+} aglDrawCommand;
+
+AGL_EXPORT(void AGL_API aglExecuteCommand( aglContext* context, const aglCommand* cmd ));
 
 #ifdef __cplusplus
 }
--- source/AGL/agl_gl.c
+++ source/AGL/agl_gl.c
@@ -32,52 +32,28 @@
 extern aglAllocCallback aglAlloc;
 extern aglFreeCallback  aglFree;
 
-typedef enum AGL_CONTEXT_TYPE {
-    AGL_OPENGL_CONTEXT   = 1,
-    AGL_DEFERRED_CONTEXT = 2,
-} AGL_CONTEXT_TYPE;
-
-struct aglContext {
-    AGL_CONTEXT_TYPE type : 8;
-};
+#include <GL/glew.h>
 
 #if defined(_WIN32)
-    #include "wagl_gl.incl"
+    #include "win_gl.incl"
 #elif defined(__linux__)
-    #include "xagl_gl.incl"
+    #include "xlib_gl.incl"
 #elif defined(__APPLE__) && defined(__MACH__)
-    #include "nsagl_gl.incl"
+    #include "ns_gl.incl"
 #else
     #error Not supported.
 #endif
 
-void AGL_API aglSetContextTitle( aglContext* context, const char* title );
-
 ////////////////////////////////////////////////////////////////////////////////
 
-struct aglCompiledCommandBuffer {
+typedef aglInputLayout {
+    
+} aglInputLayout;
 
-};
-
-aglCompiledCommandBuffer* AGL_API aglCompileCommandBuffer( const aglCommandBuffer* cmd_buffer ));
-
-void AGL_API aglExecuteCommands( aglContext* context, const aglCommandBuffer* cmd_buffer );
-void AGL_API aglExecuteCompiledCommands( aglContext* context, const aglCompiledCommandBuffer* cmd_buffer );
-
-////////////////////////////////////////////////////////////////////////////////
-
-typedef struct aglDeferredContext {
-    aglContext _context;
-    aglCommandBuffer* cmd_buffer;
-} aglDeferredContext;
-
-aglContext* AGL_API aglCreateDeferredContext( aglCommandBuffer* cmd_buffer )
+aglInputLayout* AGL_API aglCreateInputLayout( const size_t num_descs, const aglInputElementDesc* descs )
 {
-    assert(cmd_buffer);
+}
 
-    aglDeferredContext* deferred_context = (aglDeferredContext*)aglAlloc(sizeof(aglDeferredContext));
-    deferred_context->_context.type = AGL_DEFERRED_CONTEXT;
-    deferred_context->cmd_buffer = cmd_buffer;
-
-    return deferred_context;
+void AGL_API aglDestroyInputLayout( aglInputLayout* input_layout )
+{
 }
