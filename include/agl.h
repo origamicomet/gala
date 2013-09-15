496d64e76be66f5a4b1c081b1cc30ee53000d7b3
Michael 'devbug' Williams <m.t.williams@live.com>
2013-09-14 22:00:23 -0700

    Moved C++ wrapping code below respective wrapped code.

--- include/agl.h
+++ include/agl.h
@@ -78,6 +78,28 @@ extern AGL_API agl_error_handler_fn agl_error_handler();
 extern AGL_API void agl_set_error_handler(
   agl_error_handler_fn handler);
 
+/* ========================================================================== */
+
+#ifdef __cplusplus
+} /* extern "C" */
+namespace agl {
+  namespace Error {
+    /*! See agl_err_t. */
+    enum _ {
+      /*! See agl_err_t::AGL_ENONE. */
+      None        = ::AGL_ENONE,
+      /*! See agl_err_t::AGL_EINVALIDARG. */
+      InvalidArg  = ::AGL_EINVALIDARG,
+      /*! See agl_err_t::AGL_EUNKNOWN. */
+      Unknown     = ::AGL_EUNKNOWN,
+      /*! See agl_err_t::AGL_EOUTOFMEMORY. */
+      OutOfMemory = ::AGL_EOUTOFMEMORY
+    };
+  } typedef Error::_ Error_;
+} /* agl */
+extern "C" {
+#endif /* __cplusplus */
+
 /* ==========================================================================
     Allocators (agl_allocator_t):
    ========================================================================== */
@@ -112,208 +134,10 @@ extern AGL_API void agl_set_allocator(
   agl_allocator_t *allocator);
 
 /* ========================================================================== */
-/*  Common/Types:                                                             */
-/*   * Pixel Formats                                                          */
-/* ========================================================================== */
-
-/* ==========================================================================
-    Pixel Formats (agl_pixel_format_t):
-   ========================================================================== */
-
-/*! @def AGL_PIXEL_FORMAT_IS_COLOR
-  Specifies a pixel format that supports color. */
-#define AGL_PIXEL_FORMAT_IS_COLOR (1 << 31)
-
-/*! @def AGL_PIXEL_FORMAT_IS_DEPTH
-  Specifies a pixel format that supports depth. */
-#define AGL_PIXEL_FORMAT_IS_DEPTH (1 << 30)
-
-/*! @def AGL_PIXEL_FORMAT_IS_STENCIL
-  Specifies a pixel format that supports stencil. */
-#define AGL_PIXEL_FORMAT_IS_STENCIL (1 << 29)
-
-/*! @def AGL_PIXEL_FORMAT_IS_COMPRESSED
-  Specifies a pixel format that is compressed. */
-#define AGL_PIXEL_FORMAT_IS_COMPRESSED (1 << 28)
-
-/*! Specifies the format and type of pixel data. */
-typedef enum agl_pixel_format {
-  /*! An unknown pixel format. */
-  AGL_PIXEL_FORMAT_UNKNOWN = 0,
-
-  /*! A three-component, 24-bit unsigned-integer format that supports 8 bits
-    per-channel excluding alpha.
-    @note Can only be a back-buffer format. */
-  AGL_R8G8B8 = 1 |
-    AGL_PIXEL_FORMAT_IS_COLOR,
-
-  /*! A four-component, 32-bit unsigned-integer format that supports 8 bits
-    per-channel including alpha. */
-  AGL_R8G8B8A8 = 2 |
-    AGL_PIXEL_FORMAT_IS_COLOR,
-
-  /*! A 32-bit depth-buffer format that supports 24 bits for depth and 8 bits
-    for stencil. */
-  AGL_D24_S8 = 3 |
-    AGL_PIXEL_FORMAT_IS_DEPTH |
-    AGL_PIXEL_FORMAT_IS_STENCIL
-} agl_pixel_format_t;
-
-/* ========================================================================== */
-/*  Infrastructure:                                                           */
-/*   * Adapters                                                               */
-/*   * Outputs                                                                */
-/*   * Display Modes                                                          */
-/* ========================================================================== */
-
-/* ==========================================================================
-    Adapters (agl_adapter_t):
-   ========================================================================== */
-
-/* ... */
-
-/* ==========================================================================
-    Outputs (agl_outputs_t):
-   ========================================================================== */
-
-/* ... */
-
-/* ==========================================================================
-    Display Modes (agl_display_mode_t):
-   ========================================================================== */
-
-/* ... */
-
-/* ========================================================================== */
-/*  Runtime:                                                                  */
-/*   * Contexts                                                               */
-/*   * Command Lists                                                          */
-/*   * Requests                                                               */
-/*   * Resources                                                              */
-/* ========================================================================== */
-
-/* ==========================================================================
-    Contexts (agl_context_t):
-   ========================================================================== */
-
-/* ... */
-
-/* ==========================================================================
-    Command Lists (agl_command_list_t):
-   ========================================================================== */
-
-/*! A user-controlled slice of memory used to queue up commands. */
-typedef struct agl_command_list {
-  /*! @defgroup Slice @{ */
-  uintptr_t begin;
-  uintptr_t end;
-  uintptr_t current;
-  /*! @} */
-
-  /*! Called when there is not enough space left to accomodate a command.
-    @param[in,out] command_list The command list that must be resized.
-    @return True if the command list was resized to potentially accomodate the
-            command; or false if the command list was not resized. */
-  bool (*exhausted)(
-    struct agl_command_list *command_list);
-} agl_command_list_t;
-
-/* ==========================================================================
-    Requests (agl_request_t):
-   ========================================================================== */
-
-/*!  */
-typedef uint agl_request_t;
-
-/*! */
-typedef uint agl_request_response_t;
-
-/*! @def AGL_FULFILLED_REQUEST
-  Represents an answered or fulfilled request. */
-#define AGL_FULFILLED_REQUEST ((uintptr_t)0x0000000000000000ull)
-
-/*! @def AGL_UNFULFILLED_REQUEST
-  Represents an unanswered or unfulfilled request. */
-#define AGL_UNFULFILLED_REQUEST ((uintptr_t)0xffffffffffffffffull)
-
-/*! @def AGL_ERRORED_REQUEST
-  Represents an error occuring during a request. */
-#define AGL_ERRORED_REQUEST ((uintptr_t)0x0000000000000001ull)
-
-/* ==========================================================================
-    Resources (agl_resource_t):
-   ========================================================================== */
-
-/*! Specifies a resource type. */
-typedef enum agl_resource_type {
-  AGL_RESOURCE_TYPE_UNKNOWN = 0
-} agl_resource_type_t;
-
-/*! An opaque handle that represents a resource. */
-typedef struct agl_resource {
-  agl_resource_type_t _type;
-  uint _ops;
-  uintptr_t _internal;
-} agl_resource_t;
-
-/*! @def AGL_INVALID_RESOURCE
-  A invalid resource. */
-#define AGL_INVALID_RESOURCE ((agl_resource_t *)NULL)
-
-/* ========================================================================== */
-
-/*! Determines the resource type.
-  @param[in] resource The resource.
-  @returns AGL_RESOURCE_TYPE_UNKNOWN if the specifed resource is unknown or
-           non-existent; or an agl_resource_type_t. */
-extern AGL_API agl_resource_type_t agl_resource_type(
-  const agl_resource_t *resource);
-
-/*! Determines the number of pending operations.
-  @param[in] resource The resource. */
-extern AGL_API uint agl_resource_ops(
-  const agl_resource_t *resource);
-
-/* ========================================================================== */
-
-/*! Determines if the resource is existent.
-  @param[in] resource The resource.
-  @returns True if the resource is existent; or
-           false if the resource is non-existent. */
-extern AGL_API bool agl_resource_is_available(
-  const agl_resource_t *resource);
-
-/*! Determines if the resource has any outstanding operations.
-  @param[in] resource The resource.
-  @returns True if the resource has not outstanding operations; or
-           false if the resource ihas outstanding operations. */
-extern AGL_API bool agl_resource_is_reflective(
-  const agl_resource_t *resource);
 
 #ifdef __cplusplus
-}
-#endif /* __cplusplus */
-
-/* ==========================================================================
-    C++ Wrapper/Interface:
-   ========================================================================== */
-
-#ifdef __cplusplus
+} /* extern "C" */
 namespace agl {
-  namespace Error {
-    /*! See agl_err_t. */
-    enum _ {
-      /*! See agl_err_t::AGL_ENONE. */
-      None        = ::AGL_ENONE,
-      /*! See agl_err_t::AGL_EINVALIDARG. */
-      InvalidArg  = ::AGL_EINVALIDARG,
-      /*! See agl_err_t::AGL_EUNKNOWN. */
-      Unknown     = ::AGL_EUNKNOWN,
-      /*! See agl_err_t::AGL_EOUTOFMEMORY. */
-      OutOfMemory = ::AGL_EOUTOFMEMORY
-    };
-  } typedef Error::_ Error_;
-
   /*! See agl_allocator_t. */
   class Allocator {
     private:
@@ -404,7 +228,189 @@ namespace agl {
     agl_assert(debug, allocator != NULL);
     agl_set_allocator(&allocator->_);
   }
+} /* agl */
+extern "C" {
+#endif /* __cplusplus */
 
+/* ========================================================================== */
+/*  Common/Types:                                                             */
+/*   * Pixel Formats                                                          */
+/* ========================================================================== */
+
+/* ==========================================================================
+    Pixel Formats (agl_pixel_format_t):
+   ========================================================================== */
+
+/*! @def AGL_PIXEL_FORMAT_IS_COLOR
+  Specifies a pixel format that supports color. */
+#define AGL_PIXEL_FORMAT_IS_COLOR (1 << 31)
+
+/*! @def AGL_PIXEL_FORMAT_IS_DEPTH
+  Specifies a pixel format that supports depth. */
+#define AGL_PIXEL_FORMAT_IS_DEPTH (1 << 30)
+
+/*! @def AGL_PIXEL_FORMAT_IS_STENCIL
+  Specifies a pixel format that supports stencil. */
+#define AGL_PIXEL_FORMAT_IS_STENCIL (1 << 29)
+
+/*! @def AGL_PIXEL_FORMAT_IS_COMPRESSED
+  Specifies a pixel format that is compressed. */
+#define AGL_PIXEL_FORMAT_IS_COMPRESSED (1 << 28)
+
+/*! Specifies the format and type of pixel data. */
+typedef enum agl_pixel_format {
+  /*! An unknown pixel format. */
+  AGL_PIXEL_FORMAT_UNKNOWN = 0,
+
+  /*! A three-component, 24-bit unsigned-integer format that supports 8 bits
+    per-channel excluding alpha.
+    @note Can only be a back-buffer format. */
+  AGL_R8G8B8 = 1 |
+    AGL_PIXEL_FORMAT_IS_COLOR,
+
+  /*! A four-component, 32-bit unsigned-integer format that supports 8 bits
+    per-channel including alpha. */
+  AGL_R8G8B8A8 = 2 |
+    AGL_PIXEL_FORMAT_IS_COLOR,
+
+  /*! A 32-bit depth-buffer format that supports 24 bits for depth and 8 bits
+    for stencil. */
+  AGL_D24_S8 = 3 |
+    AGL_PIXEL_FORMAT_IS_DEPTH |
+    AGL_PIXEL_FORMAT_IS_STENCIL
+} agl_pixel_format_t;
+
+/* ========================================================================== */
+
+#ifdef __cplusplus
+} /* extern "C" */
+namespace agl {
+  namespace PixelFormat {
+    /*! See AGL_PIXEL_FORMAT_IS_COLOR. */
+    static const uint IsColor = AGL_PIXEL_FORMAT_IS_COLOR;
+
+    /*! See AGL_PIXEL_FORMAT_IS_DEPTH. */
+    static const uint IsDepth = AGL_PIXEL_FORMAT_IS_DEPTH;
+
+    /*! See AGL_PIXEL_FORMAT_IS_STENCIL. */
+    static const uint IsStencil = AGL_PIXEL_FORMAT_IS_STENCIL;
+
+    /*! See AGL_PIXEL_FORMAT_IS_COMPRESSED. */
+    static const uint IsCompressed = AGL_PIXEL_FORMAT_IS_COMPRESSED;
+
+    /*! See agl_pixel_format_t. */
+    enum _ {
+      /*! See agl_pixel_format_t::AGL_PIXEL_FORMAT_UNKNOWN. */
+      UNKNOWN =  ::AGL_PIXEL_FORMAT_UNKNOWN,
+      /*! See agl_pixel_format_t::AGL_R8G8B8. */
+      R8G8B8 =   ::AGL_R8G8B8,
+      /*! See agl_pixel_format_t::AGL_R8G8B8A8. */
+      R8G8B8A8 = ::AGL_R8G8B8A8,
+      /*! See agl_pixel_format_t::AGL_D24_S8. */
+      D24_S8 =   ::AGL_D24_S8
+    };
+  } typedef PixelFormat::_ PixelFormat_;
+} /* agl */
+extern "C" {
+#endif /* __cplusplus */
+
+/* ========================================================================== */
+/*  Infrastructure:                                                           */
+/*   * Adapters                                                               */
+/*   * Outputs                                                                */
+/*   * Display Modes                                                          */
+/* ========================================================================== */
+
+/* ==========================================================================
+    Adapters (agl_adapter_t):
+   ========================================================================== */
+
+/* ... */
+
+/* ==========================================================================
+    Outputs (agl_outputs_t):
+   ========================================================================== */
+
+/* ... */
+
+/* ==========================================================================
+    Display Modes (agl_display_mode_t):
+   ========================================================================== */
+
+/* ... */
+
+/* ========================================================================== */
+/*  Runtime:                                                                  */
+/*   * Contexts                                                               */
+/*   * Command Lists                                                          */
+/*   * Requests                                                               */
+/*   * Resources                                                              */
+/* ========================================================================== */
+
+/* ==========================================================================
+    Contexts (agl_context_t):
+   ========================================================================== */
+
+/* ... */
+
+/* ==========================================================================
+    Command Lists (agl_command_list_t):
+   ========================================================================== */
+
+/*! A user-controlled slice of memory used to queue up commands. */
+typedef struct agl_command_list {
+  /*! @defgroup Slice @{ */
+  uintptr_t begin;
+  uintptr_t end;
+  uintptr_t current;
+  /*! @} */
+
+  /*! Called when there is not enough space left to accomodate a command.
+    @param[in,out] command_list The command list that must be resized.
+    @return True if the command list was resized to potentially accomodate the
+            command; or false if the command list was not resized. */
+  bool (*exhausted)(
+    struct agl_command_list *command_list);
+} agl_command_list_t;
+
+/* ========================================================================== */
+
+#ifdef __cplusplus
+} /* extern "C" */
+namespace agl {
+  /*! See agl_command_list_t. */
+  typedef ::agl_command_list_t CommandList;
+} /* agl */
+extern "C" {
+#endif /* __cplusplus */
+
+/* ==========================================================================
+    Requests (agl_request_t):
+   ========================================================================== */
+
+/*!  */
+typedef uint agl_request_t;
+
+/*! */
+typedef uint agl_request_response_t;
+
+/*! @def AGL_FULFILLED_REQUEST
+  Represents an answered or fulfilled request. */
+#define AGL_FULFILLED_REQUEST ((uintptr_t)0x0000000000000000ull)
+
+/*! @def AGL_UNFULFILLED_REQUEST
+  Represents an unanswered or unfulfilled request. */
+#define AGL_UNFULFILLED_REQUEST ((uintptr_t)0xffffffffffffffffull)
+
+/*! @def AGL_ERRORED_REQUEST
+  Represents an error occuring during a request. */
+#define AGL_ERRORED_REQUEST ((uintptr_t)0x0000000000000001ull)
+
+/* ========================================================================== */
+
+#ifdef __cplusplus
+} /* extern "C" */
+namespace agl {
   /*! See agl_request_t. */
   class Request {
     private:
@@ -454,36 +460,65 @@ namespace agl {
       mutable agl_request_t _;
       agl_request_response_t _response;
   };
+} /* agl */
+extern "C" {
+#endif /* __cplusplus */
 
-  namespace PixelFormat {
-    /*! See AGL_PIXEL_FORMAT_IS_COLOR. */
-    static const uint IsColor = AGL_PIXEL_FORMAT_IS_COLOR;
+/* ==========================================================================
+    Resources (agl_resource_t):
+   ========================================================================== */
 
-    /*! See AGL_PIXEL_FORMAT_IS_DEPTH. */
-    static const uint IsDepth = AGL_PIXEL_FORMAT_IS_DEPTH;
+/*! Specifies a resource type. */
+typedef enum agl_resource_type {
+  AGL_RESOURCE_TYPE_UNKNOWN = 0,
+} agl_resource_type_t;
 
-    /*! See AGL_PIXEL_FORMAT_IS_STENCIL. */
-    static const uint IsStencil = AGL_PIXEL_FORMAT_IS_STENCIL;
+/*! An opaque handle that represents a resource. */
+typedef struct agl_resource {
+  agl_resource_type_t _type;
+  uint _ops;
+  uintptr_t _internal;
+} agl_resource_t;
 
-    /*! See AGL_PIXEL_FORMAT_IS_COMPRESSED. */
-    static const uint IsCompressed = AGL_PIXEL_FORMAT_IS_COMPRESSED;
+/*! @def AGL_INVALID_RESOURCE
+  A invalid resource. */
+#define AGL_INVALID_RESOURCE ((agl_resource_t *)NULL)
 
-    /*! See agl_pixel_format_t. */
-    enum _ {
-      /*! See agl_pixel_format_t::AGL_PIXEL_FORMAT_UNKNOWN. */
-      UNKNOWN =  ::AGL_PIXEL_FORMAT_UNKNOWN,
-      /*! See agl_pixel_format_t::AGL_R8G8B8. */
-      R8G8B8 =   ::AGL_R8G8B8,
-      /*! See agl_pixel_format_t::AGL_R8G8B8A8. */
-      R8G8B8A8 = ::AGL_R8G8B8A8,
-      /*! See agl_pixel_format_t::AGL_D24_S8. */
-      D24_S8 =   ::AGL_D24_S8
-    };
-  } typedef PixelFormat::_ PixelFormat_;
+/* ========================================================================== */
 
-  /*! See agl_command_list_t. */
-  typedef ::agl_command_list_t CommandList;
+/*! Determines the resource type.
+  @param[in] resource The resource.
+  @returns AGL_RESOURCE_TYPE_UNKNOWN if the specifed resource is unknown or
+           non-existent; or an agl_resource_type_t. */
+extern AGL_API agl_resource_type_t agl_resource_type(
+  const agl_resource_t *resource);
 
+/*! Determines the number of pending operations.
+  @param[in] resource The resource. */
+extern AGL_API uint agl_resource_ops(
+  const agl_resource_t *resource);
+
+/* ========================================================================== */
+
+/*! Determines if the resource is existent.
+  @param[in] resource The resource.
+  @returns True if the resource is existent; or
+           false if the resource is non-existent. */
+extern AGL_API bool agl_resource_is_available(
+  const agl_resource_t *resource);
+
+/*! Determines if the resource has any outstanding operations.
+  @param[in] resource The resource.
+  @returns True if the resource has not outstanding operations; or
+           false if the resource ihas outstanding operations. */
+extern AGL_API bool agl_resource_is_reflective(
+  const agl_resource_t *resource);
+
+/* ========================================================================== */
+
+#ifdef __cplusplus
+} /* extern "C" */
+namespace agl {
   /*! See agl_resource_t. */
   class Resource : private ::agl_resource_t {
     public:
