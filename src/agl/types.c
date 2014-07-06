9e641577d98c11bf36021fbbdb55132a3dd61ad4
Michael 'devbug' Williams <m.t.williams@live.com>
2014-07-05 21:04:03 -0700

    Added agl/types.h/c

---
+++ src/agl/types.c
@@ -0,0 +1,13 @@
+//===-- agl/types.c ---------------------------------------------*- C++ -*-===//
+//
+//  Abstract Graphics Library (AGL)
+//
+//  This file is distributed under the terms described in LICENSE.
+//
+//  Author(s):
+//
+//    * Michael Williams <mwilliams@bitbyte.ca>
+//
+//===----------------------------------------------------------------------===//
+
+#include "agl/types.h"
