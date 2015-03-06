05f95922f55a5a8608355dd5e6bd42579c686fd8
Michael Williams <m.t.williams@live.com>
2015-03-05 20:39:45 -0800

    Move type definitions into internal header.

--- src/gala/d3d9/backend.c
+++ src/gala/d3d9/backend.c
@@ -10,50 +10,14 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "gala/d3d9/types.h"
 #include "gala/d3d9/backend.h"
-
-//===----------------------------------------------------------------------===//
-
-#if BITBYTE_FOUNDATION_TIER0_SYSTEM == __BITBYTE_FOUNDATION_TIER0_SYSTEM_WINDOWS__
-  #define WIN32_LEAN_AND_MEAN
-  #define WIN32_EXTRA_LEAN
-
-  #include <windows.h>
-  #include <d3d9.h>
-
-  #undef WIN32_EXTRA_LEAN
-  #undef WIN32_LEAN_AND_MEAN
-
-  #undef NEAR
-  #undef FAR
-  #undef near
-  #undef far
-  #undef NO_ERROR
-  #undef ERROR
-  #undef MK_SHIFT
-  #undef MK_ALT
-  #undef min
-  #undef max
-  #undef rad1
-#else
-  #error ("Direct3D 9 is unavailable or unsupported on this platform!")
-#endif
 
 //============================================================================//
 
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
-
-//===----------------------------------------------------------------------===//
-
-struct gala_d3d9_backend {
-  gala_backend_t __backend__;
-#if BITBYTE_FOUNDATION_TIER0_SYSTEM == __BITBYTE_FOUNDATION_TIER0_SYSTEM_WINDOWS__
-  HMODULE dll;
-  IDirect3D9 *d3d9;
-#endif
-};
 
 //===----------------------------------------------------------------------===//
 
